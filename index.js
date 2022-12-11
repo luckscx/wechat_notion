242c873fc780d386b6e4952246eddd44c56aab58
grissomshen <grissomshen@tencent.com>
2022-12-11 19:06:39 +0800

    clean code

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger);
 
 const replyText = (res, FromUserName, ToUserName, CreateTime, text) => {
-  const now = Math.floor(Date.now() / 1000);
   const jsonObj = {
     ToUserName: FromUserName,
     FromUserName: ToUserName,
@@ -22,33 +21,19 @@ const replyText = (res, FromUserName, ToUserName, CreateTime, text) => {
     MsgType: "text",
     Content: text,
   }
-  console.log(jsonObj);
   res.send(jsonObj);
 };
 
 app.post("/api/json", async (req, res) => {
-  console.log('消息推送', req.body)
   const { ToUserName, FromUserName, MsgType, Content, CreateTime } = req.body
   if (MsgType === 'text') {
     if (FromUserName != "onrgDwNm0HiyRX8mEoC0AJHy2w6w") {
       replyText(res, FromUserName, ToUserName, CreateTime, "可惜不是来自Grissom主人的指令呢");
       return
     }
-    if (Content === '1111') {
-      res.send({
-        ToUserName: FromUserName,
-        FromUserName: ToUserName,
-        CreateTime: CreateTime,
-        MsgType: "text",
-        Content: "2222",
-      })
-      return
-    } else {
-      const ret_text = await notion.parseText(Content)
-      console.log(ret_text);
-      replyText(res, FromUserName, ToUserName, CreateTime, ret_text);
-      return
-    }
+    const ret_text = await notion.parseText(Content)
+    replyText(res, FromUserName, ToUserName, CreateTime, ret_text);
+    return
   } else {
     // for 接口配置测试
     res.send('success')
