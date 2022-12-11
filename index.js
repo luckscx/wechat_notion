606fd4ff9ff96e35d6e0fee47879a891f90493b3
grissomshen <grissomshen@tencent.com>
2022-12-11 18:59:12 +0800

    remove xml

--- index.js
+++ index.js
@@ -2,67 +2,55 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const notion = require("./notion.js");
-const convert = require('xml-js');
-const xmlparser = require('express-xml-bodyparser');
 const bodyParser = require('body-parser')
 
 const logger = morgan("tiny");
 
 const app = express();
 app.use(cors());
-app.use(express.urlencoded({extended : true}));
-app.use(xmlparser({normalizeTags : false, explicitArray : false}));
 app.use(bodyParser.raw())
 app.use(bodyParser.json({}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger);
 
-const replyText = (res, FromUserName, ToUserName, text) => {
+const replyText = (res, FromUserName, ToUserName, CreateTime, text) => {
   const now = Math.floor(Date.now() / 1000);
   const jsonObj = {
     ToUserName: FromUserName,
     FromUserName: ToUserName,
-    CreateTime: now,
+    CreateTime: CreateTime,
     MsgType: "text",
     Content: text,
   }
-  var options = {compact: true, ignoreComment: true, spaces: 0};
-  const xmlObj = convert.json2xml(jsonObj, options)
-  res.type('application/xml');
-  res.send(xmlObj);
+  console.log(jsonObj);
+  res.send(jsonObj);
 };
-
-app.post("/api/text", async (req, res) => {
-  console.log("Get PostBody", req.body);
-  const { ToUserName, FromUserName, MsgType, Content} = req.body.root;
-  if (FromUserName != "onrgDwNm0HiyRX8mEoC0AJHy2w6w") {
-    replyText(res, FromUserName, ToUserName, "可惜不是来自Grissom主人的指令呢");
-    return
-  }
-  if (MsgType === "text") {
-    const ret_text = await notion.parseText(Content);
-    replyText(res, FromUserName, ToUserName, ret_text);
-  } else {
-    res.send("success");
-  }
-});
 
 app.post("/api/json", async (req, res) => {
   console.log('消息推送', req.body)
   const { ToUserName, FromUserName, MsgType, Content, CreateTime } = req.body
   if (MsgType === 'text') {
-    if (Content === '回复文字') {
+    if (FromUserName != "onrgDwNm0HiyRX8mEoC0AJHy2w6w") {
+      replyText(res, FromUserName, ToUserName, CreateTime, "可惜不是来自Grissom主人的指令呢");
+      return
+    }
+    if (Content === '1111') {
       res.send({
         ToUserName: FromUserName,
         FromUserName: ToUserName,
         CreateTime: CreateTime,
-        MsgType: 'text',
-        Content: '这是回复的消息'
+        MsgType: "text",
+        Content: "2222",
       })
+      return
     } else {
-      res.send("success")
+      const ret_text = await notion.parseText(Content)
+      console.log(ret_text);
+      replyText(res, FromUserName, ToUserName, CreateTime, ret_text);
+      return
     }
   } else {
+    // for 接口配置测试
     res.send('success')
   }
 });
