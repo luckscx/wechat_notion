816836da65749baf31870c4aa6537b8d59f687bf
grissomshen <grissomshen@tencent.com>
2024-11-02 11:02:24 +0800

    up

--- notion.js
+++ notion.js
@@ -78,7 +78,7 @@ function makeNewBuyItemPage(target_title) {
   return {
     Name: {
       type: 'title',
-      title: [{type: 'text', text: {content: target_title}}],
+      title: [{ type: 'text', text: { content: target_title } }],
     },
     状态: {
       type: 'select',
@@ -97,18 +97,18 @@ function makeNewBuyItemPage(target_title) {
 
 const add_page_func_factory = async function (name, input_text, db_id, prop) {
   const new_page = {
-    parent: {database_id: db_id},
-    properties: prop
+    parent: { database_id: db_id },
+    properties: prop,
   };
-  console.log(new_page)
+  console.log(new_page);
   try {
-    await myRetry(async () => await notion.pages.create(new_page));
+    await myRetry(async () => notion.pages.create(new_page));
     return `添加${name} [${input_text}] 成功`;
   } catch (error) {
     console.log(error);
     return `添加${name} [${input_text}] 失败，请检查日志`;
   }
-}
+};
 
 const addTodo = async (from_text) => {
   const input_text = from_text.replace('todo', '');
@@ -121,89 +121,85 @@ const addTodo = async (from_text) => {
   return '添加ToDo失败，需检查日志';
 };
 
-const check_func_factory = (pre_key_ar) => {
-  return (cmd) => {
-    for (let i = 0; i < pre_key_ar.length; i += 1) {
-      const pre_key = pre_key_ar[i];
-      if (cmd.startsWith(pre_key)) {
-        console.log("hit rule")
-        const regex = new RegExp(`^${pre_key}`, 'i');
-        return cmd.replace(regex, '');
-      }
+const check_func_factory = (pre_key_ar) => (cmd) => {
+  for (let i = 0; i < pre_key_ar.length; i += 1) {
+    const pre_key = pre_key_ar[i];
+    if (cmd.startsWith(pre_key)) {
+      console.log('hit rule');
+      const regex = new RegExp(`^${pre_key}`, 'i');
+      return cmd.replace(regex, '');
     }
-    return null;
-  };
+  }
+  return null;
 };
-
 
 const get_template_page = async (db_id) => {
   const response = await notion.databases.query({
     database_id: db_id,
     sorts: [
       {
-        property: "日期",
-        direction: "descending"
-      }
+        property: '日期',
+        direction: 'descending',
+      },
     ],
-    page_size : 1,
+    page_size: 1,
   });
-  const last_page = response.results[0]
-  return last_page.properties
-}
-
+  const last_page = response.results[0];
+  return last_page.properties;
+};
 
 const check_arr = [{
-  name : "任务项",
-  pre_key_ar : ["task","任务"],
-  db_id : task_db_id,
-  property_maker : makeNewTaskPage,
+  name: '任务项',
+  pre_key_ar: ['task', '任务'],
+  db_id: task_db_id,
+  property_maker: makeNewTaskPage,
 }, {
-  name : "购物项",
-  pre_key_ar : ["buy","购物"],
-  db_id : buy_db_id,
-  property_maker : makeNewBuyItemPage,
+  name: '购物项',
+  pre_key_ar: ['buy', '购物'],
+  db_id: buy_db_id,
+  property_maker: makeNewBuyItemPage,
 }, {
-  name : "奇思妙想",
-  pre_key_ar : ["idea","我想"],
-  db_id : idea_db_id,
+  name: '奇思妙想',
+  pre_key_ar: ['idea', '我想'],
+  db_id: idea_db_id,
 }, {
-  name : "关系人",
-  pre_key_ar : ["people","关系人"],
-  db_id : people_db_id,
+  name: '关系人',
+  pre_key_ar: ['people', '关系人'],
+  db_id: people_db_id,
 }, {
-  name : "Session记录",
-  pre_key_ar : ["poker","德州"],
-  db_id : poker_db_id,
-  template_page : true, // 是否拉取上一页作为模板
-  property_maker : (in_text, base_props) => {
-    delete base_props["实际盈利"]
-    delete base_props["每小时盈利"]
-    base_props["日期"]["date"].start = moment.utc().format('YYYY-MM-DD HH:mm:ss')
-    base_props["结果"]["number"] = parseInt(in_text)
-    return base_props
-  }
-}]
+  name: 'Session记录',
+  pre_key_ar: ['poker', 'pk'],
+  db_id: poker_db_id,
+  template_page: true, // 是否拉取上一页作为模板
+  property_maker: (in_text, base_props) => {
+    delete base_props['实际盈利'];
+    delete base_props['每小时盈利'];
+    base_props['日期'].date.start = moment.utc().format('YYYY-MM-DD HH:mm');
+    base_props['结果'].number = parseInt(in_text);
+    return base_props;
+  },
+}];
 
 // 回包文本
 async function parseText(from_text) {
   for (let i = 0; i < check_arr.length; i++) {
-    const check_obj = check_arr[i]
-    const check_func = check_func_factory(check_obj.pre_key_ar)
-    const input_text = check_func(from_text)
+    const check_obj = check_arr[i];
+    const check_func = check_func_factory(check_obj.pre_key_ar);
+    const input_text = check_func(from_text);
     if (input_text) {
       let base_props = {
         Name: {
           type: 'title',
-          title: [{type: 'text', text: {content: input_text}}],
+          title: [{ type: 'text', text: { content: input_text } }],
         },
-      }
+      };
       if (check_obj.template_page) {
-        base_props = await get_template_page(check_obj.db_id)
+        base_props = await get_template_page(check_obj.db_id);
       }
       if (check_obj.property_maker) {
-        base_props = check_obj.property_maker(input_text, base_props)
+        base_props = check_obj.property_maker(input_text, base_props);
       }
-      return await add_page_func_factory(check_obj.name, input_text, check_obj.db_id, base_props)
+      return add_page_func_factory(check_obj.name, input_text, check_obj.db_id, base_props);
     }
   }
   return addTodo(from_text);
